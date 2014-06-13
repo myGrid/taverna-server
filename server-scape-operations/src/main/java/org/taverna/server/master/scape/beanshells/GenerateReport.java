c3e50577aca53867ca8a365200adb4d51e73ed02
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2014-06-13 15:09:55 +0100

    Fix report generation, finish converting workflows.

--- server-scape-operations/src/main/java/org/taverna/server/master/scape/beanshells/GenerateReport.java
+++ server-scape-operations/src/main/java/org/taverna/server/master/scape/beanshells/GenerateReport.java
@@ -21,15 +21,16 @@ public class GenerateReport extends Support<GenerateReport> {
 		Iterator<String> ob_it = objects.iterator();
 		Iterator<String> wi_it = writtenInfo.iterator();
 		Iterator<String> we_it = writeErrors.iterator();
-		Iterator<List<String>> ae_it = assessErrors.iterator();
+		Iterator<List<String>> ae_it = (assessErrors != null ? assessErrors
+				.iterator() : null);
 		StringBuilder errorsBuffer = new StringBuilder();
 		StringBuilder writtenBuffer = new StringBuilder("<h1>Written:</h1><ul>");
 		nout = nerr = 0;
 
 		while (ob_it.hasNext() && wi_it.hasNext() && we_it.hasNext()
-				&& ae_it.hasNext())
-			generateReportLine(errorsBuffer, writtenBuffer, doWrite,
-					ob_it.next(), wi_it.next(), we_it.next(), ae_it.next());
+				&& (ae_it == null || ae_it.hasNext()))
+			generateReportLine(errorsBuffer, writtenBuffer, next(ob_it),
+					next(wi_it), next(we_it), next(ae_it));
 		report = format(
 				"<h2>Summary</h2>There were %s successful writes and %d errors."
 						+ "<h2>Written</h2><ul>%s<ul>"
@@ -37,36 +38,47 @@ public class GenerateReport extends Support<GenerateReport> {
 				writtenBuffer, errorsBuffer);
 	}
 
+	private static <T> T next(Iterator<T> iterator) {
+		return iterator == null ? null : iterator.next();
+	}
+
 	private void generateReportLine(StringBuilder errorsBuffer,
-			StringBuilder writtenBuffer, boolean reallyWrite, String ob,
-			String wi, String we, List<String> ae) {
-		if (!ae.isEmpty()) {
+			StringBuilder writtenBuffer, String ob, String wi, String we,
+			List<String> ae) {
+		// Errors in assessment
+		if (ae != null && !ae.isEmpty()) {
 			errorsBuffer.append("<li>Object ").append(ob)
 					.append(" failed assessment.<ul>");
 			for (String e : ae)
 				errorsBuffer.append("<li>").append(e).append("</li>");
 			errorsBuffer.append("</ul></li>");
 			nerr++;
-		} else if (we != null && !we.isEmpty()) {
+			return;
+		}
+
+		// Errors in write
+		if (we != null && !we.isEmpty()) {
 			errorsBuffer.append("<li>Object ").append(ob)
 					.append(" failed in upload.<br>").append(we)
 					.append("</ul></li>");
 			nerr++;
-		} else {
-			String[] info = wi.split(";", 2);
-			String src = info[0];
-			String repo = info[1];
-			writtenBuffer.append("<li>Object ").append(ob);
-			String tail = "";
-			if (reallyWrite)
-				writtenBuffer.append(" was written back to ");
-			else {
-				tail = " (write-back inhibited)";
-				writtenBuffer.append(" would have been written back to ");
-			}
-			writtenBuffer.append(src).append(" in repository ")
-					.append(repo).append(tail).append("</li>");
-			nout++;
+			return;
 		}
+
+		// Report success
+		String[] info = wi.split(";", 2);
+		String src = info[0];
+		String repo = info[1];
+		writtenBuffer.append("<li>Object ").append(ob);
+		String tail = "";
+		if (doWrite)
+			writtenBuffer.append(" was written back to ");
+		else {
+			tail = " (write-back was inhibited by policy)";
+			writtenBuffer.append(" would have been written back to ");
+		}
+		writtenBuffer.append(src).append(" in repository ").append(repo)
+				.append(tail).append("</li>");
+		nout++;
 	}
 }
