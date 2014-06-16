6f37449316c0a83c4cdd53e7cda965fb8fa658ee
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2014-06-16 14:22:50 +0100

    More tidying up

--- server-scape-operations/src/main/java/org/taverna/server/master/scape/beanshells/AssessMetrics.java
+++ server-scape-operations/src/main/java/org/taverna/server/master/scape/beanshells/AssessMetrics.java
@@ -22,9 +22,13 @@ public class AssessMetrics extends Support<AssessMetrics> {
 	@Input
 	private String metrics;
 	@Output
-	private List<String> failures;
+	private List<String> failures = new ArrayList<>();
+	@Output
+	private List<String> successes = new ArrayList<>();
 	@Output
 	private boolean isSatisfied;
+
+	private static final String NS = "http://purl.oclc.org/dsdl/svrl";
 
 	@Override
 	public void perform() throws Exception {
@@ -32,33 +36,53 @@ public class AssessMetrics extends Support<AssessMetrics> {
 		File schematron = new File("schematron_" + id + ".xml");
 		File measuresDoc = new File("measures_" + id + ".xml");
 
-		try (PrintWriter pw = new PrintWriter(schematron, "UTF-8")) {
-			pw.println(QLD);
-		}
-		try (PrintWriter pw = new PrintWriter(measuresDoc, "UTF-8")) {
-			pw.println(metrics);
-		}
-		failures = new ArrayList<>();
-
 		try {
-			ISchematronResource validator = fromFile(schematron);
-			if (!validator.isValidSchematron())
-				throw new Exception("invalid schematron document");
-			Document result = validator
-					.applySchematronValidation(new StreamSource(measuresDoc));
-			NodeList nl = result.getDocumentElement().getElementsByTagNameNS(
-					"http://purl.oclc.org/dsdl/svrl", "failed-assert");
-			for (int i = 0; i < nl.getLength(); i++) {
-				Element elem = (Element) nl.item(i);
-				failures.add(elem
-						.getElementsByTagNameNS(
-								"http://purl.oclc.org/dsdl/svrl", "text")
-						.item(0).getTextContent());
+			try (PrintWriter pw = new PrintWriter(schematron, "UTF-8")) {
+				pw.println(QLD);
 			}
-			isSatisfied = failures.isEmpty();
+			try {
+				try (PrintWriter pw = new PrintWriter(measuresDoc, "UTF-8")) {
+					pw.println(metrics);
+				}
+				matchSchematronToDocument(schematron, new StreamSource(
+						measuresDoc));
+			} finally {
+				measuresDoc.delete();
+			}
 		} finally {
-			measuresDoc.delete();
 			schematron.delete();
+		}
+	}
+
+	private void matchSchematronToDocument(File schematron,
+			StreamSource measuresDoc) throws Exception {
+		// Construct the validator
+		ISchematronResource validator = fromFile(schematron);
+		if (!validator.isValidSchematron())
+			throw new Exception("invalid schematron document");
+
+		// Perform the validation
+		Document result = validator.applySchematronValidation(measuresDoc);
+
+		// Parse the results
+		getAssessmentDescriptions(failures, result, "failed-assert",
+				"undescribed failure");
+		getAssessmentDescriptions(successes, result, "successful-report",
+				"undescribed success");
+		isSatisfied = failures.isEmpty();
+	}
+
+	private void getAssessmentDescriptions(List<String> collector,
+			Document result, String container, String defaultMessage) {
+		NodeList nl = result.getDocumentElement().getElementsByTagNameNS(NS,
+				container);
+		for (int i = 0; i < nl.getLength(); i++) {
+			Element elem = (Element) nl.item(i);
+			NodeList text = elem.getElementsByTagNameNS(NS, "text");
+			if (text.getLength() == 0)
+				collector.add(defaultMessage);
+			else
+				collector.add(text.item(0).getTextContent());
 		}
 	}
 }
