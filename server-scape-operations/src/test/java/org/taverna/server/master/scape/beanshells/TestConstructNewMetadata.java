23a2ffd2d233b4370850dae56a7123940c0c36dc
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2014-06-16 14:07:10 +0100

    Better testing

--- server-scape-operations/src/test/java/org/taverna/server/master/scape/beanshells/TestConstructNewMetadata.java
+++ server-scape-operations/src/test/java/org/taverna/server/master/scape/beanshells/TestConstructNewMetadata.java
@@ -1,16 +1,21 @@
 package org.taverna.server.master.scape.beanshells;
 
 import static java.nio.file.Files.readAllBytes;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestConstructNewMetadata {
-	String originalMetadata;
-	String newInformation;
+	private String originalMetadata;
+	private String newInformation;
+	private String createdMetadata;
 
 	@Before
 	public void loadData() throws Exception {
@@ -18,6 +23,24 @@ public class TestConstructNewMetadata {
 				.getResource("/example_original.xml").toURI())));
 		newInformation = new String(readAllBytes(Paths.get(getClass()
 				.getResource("/example_newinfo.xml").toURI())));
+		createdMetadata = new String(readAllBytes(Paths.get(getClass()
+				.getResource("/example_out.xml").toURI())));
+	}
+
+	private static final Pattern UUID = Pattern
+			.compile("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}");
+
+	private String mapUUIDs(String s) {
+		Matcher m = UUID.matcher(s);
+		StringBuffer buffer = new StringBuffer();
+		Map<String, Integer> map = new HashMap<>();
+		while (m.find()) {
+			Integer i = map.get(m.group());
+			if (i == null)
+				map.put(m.group(), i = map.size());
+			m.appendReplacement(buffer, "UUID-" + i);
+		}
+		return m.appendTail(buffer).toString();
 	}
 
 	@Test
@@ -29,8 +52,7 @@ public class TestConstructNewMetadata {
 				.init("originalMetadata", originalMetadata)
 				.init("newInformation", newInformation);
 		op.perform();
-		System.out.println(op.getResult("newMetadata"));
-		assertNotEquals("", op.getResult("newMetadata"));
+		assertEquals(mapUUIDs(createdMetadata), mapUUIDs(op.getResult("newMetadata").toString()));
 	}
 
 }
