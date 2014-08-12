ed1393e7a0126e1e92a070dc6a713d8ef8da9e1d
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2014-08-12 09:57:08 +0100

    Improve the documentation.

--- server-scape-operations/src/main/java/org/taverna/server/master/scape/beanshells/BeanshellSupport.java
+++ server-scape-operations/src/main/java/org/taverna/server/master/scape/beanshells/BeanshellSupport.java
@@ -13,6 +13,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 /**
  * Instances of this interface are used to help write the beanshell scripts
  * inside the outer workflows.
@@ -21,28 +24,87 @@ import java.util.Map.Entry;
  */
 
 public interface BeanshellSupport<T extends BeanshellSupport<?>> {
+	/**
+	 * Carry out the operation defined by this object. The {@linkplain Input
+	 * inputs} must be defined before this operation is called, and the
+	 * {@linkplain Output outputs} will be readable afterwards. Should only be
+	 * called once per <i>instance</i> of this class.
+	 * 
+	 * @throws IllegalStateException
+	 *             If a required input has not set before calling this method.
+	 * @throws Exception
+	 *             If anything goes wrong.
+	 */
 	void perform() throws Exception;
 
-	T init(String name, Object value);
+	/**
+	 * Write to the {@linkplain Input input} variable called <tt>name</tt>.
+	 * 
+	 * @param name
+	 *            The input variable to write.
+	 * @param value
+	 *            The value to write. Must be of the right type for the variable
+	 *            (or <tt>null</tt>, if permitted).
+	 * @return <tt>this</tt>, to enable fluent use of this API.
+	 * @throws UnsupportedOperationException
+	 *             If that variable is not writable (e.g., if it doesn't exist
+	 *             or the types don't match).
+	 */
+	@Nonnull
+	T init(@Nonnull String name, @Nullable Object value);
 
-	Object getResult(String name);
+	/**
+	 * Read the {@linkplain Output output} variable called <tt>name</tt>.
+	 * 
+	 * @param name
+	 *            The output variable to read.
+	 * @return The value that was in the variable. This may be any type and may
+	 *         be <tt>null</tt>.
+	 * @throws UnsupportedOperationException
+	 *             If that variable is not readable (e.g., if it doesn't exist).
+	 */
+	@Nullable
+	Object getResult(@Nonnull String name);
 
+	/**
+	 * Indicates that a field corresponds to a beanshell input.
+	 * 
+	 * @author Donal Fellows
+	 */
 	@Target(FIELD)
 	@Retention(RUNTIME)
 	public @interface Input {
+		/** Is this a required input? Defaults to yes. */
 		boolean required() default true;
+
+		/** Is this a nullable input? Defaults to no. */
 		boolean nullable() default false;
 	}
 
+	/**
+	 * Indicates that a field corresponds to a beanshell output.
+	 * 
+	 * @author Donal Fellows
+	 */
 	@Target(FIELD)
 	@Retention(RUNTIME)
 	public @interface Output {
-	}	
+	}
 }
 
+/**
+ * Micro-framework for implementing the support, which handles mapping access
+ * from the beanshell to the annotated fields, and also enforces whether a
+ * required parameter is supplied or not.
+ * 
+ * @param <T>
+ *            Should be the subclass of this class.
+ * @author Donal Fellows
+ */
 abstract class Support<T extends BeanshellSupport<?>> implements
 		BeanshellSupport<T> {
-	private Map<String,Field> inputs = new HashMap<>(), outputs = new HashMap<>();
+	private Map<String, Field> inputs = new HashMap<>(),
+			outputs = new HashMap<>();
 
 	protected abstract void op() throws Exception;
 
@@ -65,6 +127,7 @@ abstract class Support<T extends BeanshellSupport<?>> implements
 	}
 
 	protected Support() {
+		// Oh god! What a horrible hack! (Makes JAXB work in Taverna.)
 		currentThread().setContextClassLoader(getClass().getClassLoader());
 		for (Field f : getClass().getDeclaredFields()) {
 			if (f.getAnnotation(Input.class) != null) {
@@ -99,7 +162,8 @@ abstract class Support<T extends BeanshellSupport<?>> implements
 		} catch (NumberFormatException e) {
 			throw e;
 		} catch (Exception e) {
-			throw new UnsupportedOperationException("could not write to field " + name, e);
+			throw new UnsupportedOperationException("could not write to field "
+					+ name, e);
 		}
 	}
 
@@ -115,7 +179,8 @@ abstract class Support<T extends BeanshellSupport<?>> implements
 				return f.getInt(this);
 			return f.get(this);
 		} catch (Exception e) {
-			throw new UnsupportedOperationException("could not read from field " + name, e);
+			throw new UnsupportedOperationException(
+					"could not read from field " + name, e);
 		}
 	}
 }
